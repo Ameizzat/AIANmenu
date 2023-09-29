1debfda35d924451bf3412505824d1fc94822f3b
Ameizzat <146401458+Ameizzat@users.noreply.github.com>
2023-09-30 02:47:01 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+// JavaScript for quantity control
+document.addEventListener('DOMContentLoaded', function () {
+  // Get all quantity boxes and buttons
+  const quantityBoxes = document.querySelectorAll('.quantity-box');
+  const plusButtons = document.querySelectorAll('.plus');
+  const minusButtons = document.querySelectorAll('.minus');
+  const selectedItems = [];
+
+  // Attach event listeners to plus and minus buttons
+  plusButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      // JavaScript for plus button
+    });
+  });
+
+  minusButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      // JavaScript for minus button
+    });
+  });
+
+  // Function to update the order list and calculate total price
+  function updateOrderList() {
+    // JavaScript for updating order list and total price
+  }
+});
