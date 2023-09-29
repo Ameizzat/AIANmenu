045a25462b2b3bce8c06744a1727325f8c9f72bc
Ameizzat <146401458+Ameizzat@users.noreply.github.com>
2023-09-30 03:07:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,18 +9,73 @@ document.addEventListener('DOMContentLoaded', function () {
   // Attach event listeners to plus and minus buttons
   plusButtons.forEach((button, index) => {
     button.addEventListener('click', () => {
-      // JavaScript for plus button
+      const quantityElement = quantityBoxes[index].querySelector('.quantity');
+      let quantity = parseInt(quantityElement.textContent);
+      quantity += 1;
+      quantityElement.textContent = quantity;
+
+      // Add the item index to the selectedItems array
+      if (quantity === 1) {
+        selectedItems.push(index);
+      }
+      updateOrderList();
     });
   });
 
   minusButtons.forEach((button, index) => {
     button.addEventListener('click', () => {
-      // JavaScript for minus button
+      const quantityElement = quantityBoxes[index].querySelector('.quantity');
+      let quantity = parseInt(quantityElement.textContent);
+      if (quantity > 0) {
+        quantity -= 1;
+        quantityElement.textContent = quantity;
+
+        // Remove the item index from the selectedItems array
+        if (quantity === 0) {
+          const itemIndex = selectedItems.indexOf(index);
+          if (itemIndex !== -1) {
+            selectedItems.splice(itemIndex, 1);
+          }
+        }
+        updateOrderList();
+      }
     });
   });
 
   // Function to update the order list and calculate total price
   function updateOrderList() {
-    // JavaScript for updating order list and total price
+    const orderList = document.querySelector('#order-list ul');
+    const receiptList = document.querySelector('.receipt ul');
+    receiptList.innerHTML = '';
+
+    let totalPrice = 0; // Initialize total price
+
+    selectedItems.forEach((index) => {
+      const item = document.querySelectorAll('.menu-item')[index];
+      const itemName = item.querySelector('span').textContent;
+      const itemPrice = parseFloat(item.querySelector('.unit-price').textContent.replace('RM ', ''));
+
+      // Get the quantity for the current item
+      const quantity = parseInt(quantityBoxes[index].querySelector('.quantity').textContent);
+
+      // Calculate the item total
+      const itemTotal = itemPrice * quantity;
+
+      totalPrice += itemTotal; // Add item total to the total price
+
+      const listItem = document.createElement('li');
+      listItem.className = 'receipt-item';
+      listItem.innerHTML = `
+        <span class="item-quantity">${quantity}x</span>
+        <span class="item-name-space">&nbsp;</span>
+        <span class="item-name">${itemName}</span>
+        <span class="item-price">RM ${itemTotal.toFixed(2)}</span>
+      `;
+      receiptList.appendChild(listItem);
+    });
+
+    // Update the total price display
+    const totalPriceDiv = document.getElementById('total-price');
+    totalPriceDiv.textContent = `Total Price: RM ${totalPrice.toFixed(2)}`;
   }
 });
